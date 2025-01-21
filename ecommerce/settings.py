b94d8fdb02ce110d60ba53651fe09b5a70b20cb3
Zin_Ko <zinkolaymgy@gmail.com>
2025-01-21 16:57:57 +0700

    data

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -90,8 +90,8 @@ DATABASES = {
     }
 }
 
-database_url = os.environ.get('DATABASE_URL')
-DATABASES['default']=dj_database_url.parse(database_url)
+database_url = os.environ.get("DATABASE_URL")
+DATABASES['default'] = dj_database_url.parse(database_url)
 
 
 # Password validation
